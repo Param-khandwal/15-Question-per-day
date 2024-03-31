5251eebb09233beec5e33901c666aca6cf1344b2
PARDEEP KUMAR <pramkhandwal1766@gmail.com>
2024-03-31 12:55:52 +0530

    updates files

--- Dsa Practice/Array_Operation/Array.c
+++ Dsa Practice/Array_Operation/Array.c
@@ -1,9 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+// #define myArr[100]
+
 int main()
 {
+    int *p, n, i;
+    printf("Enter the number of int to be entered");
+    scanf("%d", &n);
+    p = (int *)malloc(n * sizeof(int));
 
+    if (p == NULL)
+    {
+        printf("memory not available");
+        exit(1);
+    }
 
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter integer :  ");
+        scanf("%d",&p[i]);
+    }
 
+    for (int i = 0; i < n; i++)
+    {
 
-
+        printf("%d\t", p[i]);
+    }
 }
--- Dsa Practice/Array_Operation/Sum_of_subarr.c
+++ Dsa Practice/Array_Operation/Sum_of_subarr.c
@@ -2,17 +2,16 @@
 // #define arr [100]
 int main(){
 
-    int target=6,sum=0;
+    int target=6;
     
      int arr[5]={1,2,3,4,5};
 
     for(int i=0;i<5;i++){
         for(int j=i+1;j<5;j++){
             if(arr[i]+arr[j]==target){
-                sum++;  
+                printf("arr[i]=%d arr[j]=%d\n",arr[i],arr[j]);
             }
         }
     }
-    printf("%d",sum);
-
+    return 0;
 }
