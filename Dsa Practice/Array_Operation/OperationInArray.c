feccb802934a219de290c1a1ce3686736d6d300d
PARDEEP KUMAR <pramkhandwal1766@gmail.com>
2024-04-03 19:31:58 +0530

    updation

--- Dsa Practice/Array_Operation/OperationInArray.c
+++ Dsa Practice/Array_Operation/OperationInArray.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct myArray
 {
     int total_size; // total size of the array
@@ -9,14 +8,12 @@ struct myArray
     int *ptr;
 };
 
-
 void createArr(struct myArray *a, int tsize, int usize)
 {
     a->total_size = tsize;
     a->used_size = usize;
     a->ptr = (int *)malloc(tsize * sizeof(int));
 } // creation of array using malloc
-
 
 void show(struct myArray *a)
 {
@@ -27,6 +24,20 @@ void show(struct myArray *a)
     }
 } // end of the display function
 
+int insertion(struct myArray *a, int n, int usize, int tsize,int element)
+{
+    if (usize >= tsize)
+    {
+        return -1;
+    }
+    for (int i = 0; i < a->used_size; i++)
+    {
+        (a->ptr)[i+1]=((a)->ptr)[i];
+    }
+    ((a)->ptr)[usize] = element;
+    a->used_size++;
+    return 1;
+}
 
 void inputValue(struct myArray *a)
 {
@@ -41,11 +52,11 @@ void inputValue(struct myArray *a)
 
 int main()
 {
-   struct myArray arr;
-   createArr(&arr,5,3);
-   printf("The elements are:\n");
-   inputValue(&arr);
-   show( &arr );
-   return 0;
-
+    struct myArray arr;
+    createArr(&arr, 5, 3);
+    printf("The elements are:\n");
+    inputValue(&arr);
+    insertion(&arr,5,2,4,-9);
+    show(&arr);
+    return 0;
 }
