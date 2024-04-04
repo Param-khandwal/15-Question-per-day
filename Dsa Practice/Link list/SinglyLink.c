a4cf951e20f6de60c6498c528190f88f94fa73a5
PARDEEP KUMAR <pramkhandwal1766@gmail.com>
2024-04-04 06:57:51 +0530

    updating

---
+++ Dsa Practice/Link list/SinglyLink.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *next;
+    struct node *head, *newnode, *temp;
+     head = NULL;
+    int choice=1;
+
+    while (choice)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("\n Enter the data of the node");
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+        temp = (struct node *)malloc(sizeof(struct node));
+       
+        if (head == NULL)
+        {
+            head = temp = newnode;
+        }
+
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+
+       // printf("\nEnter your choice (0,1)");
+       // scanf("%d", &choice);
+
+        while (temp!=NULL)     
+        {
+            printf("%d",temp->data);
+            temp=temp->next;
+        }
+        
+           // return 0;
+    }
+}
